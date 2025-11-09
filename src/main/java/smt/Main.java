267eab9d864eb117ccc9c6fe5097c498e0b6fec6
mta117 <mta117@sfu.ca>
2025-11-08 21:55:06 -0800

    Initial commit

---
+++ src/main/java/smt/Main.java
@@ -0,0 +1,12 @@
+package smt;
+
+import com.microsoft.z3.*;
+
+import java.io.*;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
